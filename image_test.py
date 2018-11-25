bfe1c36a58d55980afc1f88e97f2cdf6a1504360
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2018-11-25 13:20:03 -0800

    Function

--- image_test.py
+++ image_test.py
@@ -4,41 +4,40 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from os import listdir
+
+def channel_merge(x, b, b_coeff, g, g_coeff, r, r_coeff, workDir):
+    # adaptive equalizing
+    clahe = cv2.createCLAHE(clipLimit=4.0, tileGridSize=(32, 32))
+    cl1 = clahe.apply(b)
+    cl2 = clahe.apply(g)
+    cl3 = clahe.apply(r)
+
+    # combine channels
+    img = np.zeros((b.shape[0], b.shape[1], 3))
+    img[:, :, 0] = cl1 / b_coeff
+    img[:, :, 1] = cl2 / g_coeff
+    img[:, :, 2] = cl3 / r_coeff
+
+    # plot histograms for every channel
+    color = ('b', 'g', 'r')
+    for i, col in enumerate(color):
+        histr = cv2.calcHist([lol], [i], None, [256], [0, 256])
+        plt.plot(histr, color=col)
+        plt.xlim([0, 256])
+    plt.show()
+
+    # write image
+    cv2.imwrite("/".join([workDir, x]), img)
+
 
 # import images
-b = cv2.imread("/Users/ogorodnikov/Box Sync/Pheno-Sudoku/Opera Phoenix 100318/HEK293 cytokinesis test__2018-10-03T16_10_58-Measurement 1/Images/r01c01f01p01-ch1sk1fk1fl1.tiff", -1)
-g = cv2.imread("/Users/ogorodnikov/Box Sync/Pheno-Sudoku/Opera Phoenix 100318/HEK293 cytokinesis test__2018-10-03T16_10_58-Measurement 1/Images/r01c01f01p01-ch2sk1fk1fl1.tiff", -1)
-r = cv2.imread("/Users/ogorodnikov/Box Sync/Pheno-Sudoku/Opera Phoenix 100318/HEK293 cytokinesis test__2018-10-03T16_10_58-Measurement 1/Images/r01c01f01p01-ch3sk1fk1fl1.tiff", -1)
+b = cv2.imread("/Users/antonogorodnikov/Desktop/img/r01c01f01p01-ch1sk1fk1fl1.tiff", -1)
+g = cv2.imread("/Users/antonogorodnikov/Desktop/img/r01c01f01p01-ch2sk1fk1fl1.tiff", -1)
+r = cv2.imread("/Users/antonogorodnikov/Desktop/img/r01c01f01p01-ch3sk1fk1fl1.tiff", -1)
 
 
-clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-cl1 = clahe.apply(b)
-cl2 = clahe.apply(g)
-cl3 = clahe.apply(r)
+20, 100, 50
 
-needed_multi_channel_img = np.zeros((b.shape[0], b.shape[1], 3))
-needed_multi_channel_img [:,:,0] = b/5
-needed_multi_channel_img [:,:,1] = g/5
-needed_multi_channel_img [:,:,2] = r/5
-
-img = cv2.merge((b, g, r))
-
-
-plt.hist(img.ravel(),256,[0,256]); plt.show()
-
-
-
-
-
-plt.imshow(needed_multi_channel_img)
-plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-plt.show()
-
-
-cv2.imwrite('/Users/ogorodnikov/Desktop/b.tiff',needed_multi_channel_img)
-
-
-equ = cv2.equalizeHist(b)
-res = np.hstack((b,equ)) #stacking images side-by-side
-cv2.imwrite('/Users/ogorodnikov/Desktop/res.tiff',b)
-
+direct = listdir("/Users/antonogorodnikov/Desktop/img")
+for x in
