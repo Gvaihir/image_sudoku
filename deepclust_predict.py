3b3e4a877b272f1229e2b9d47b215a539e47a9c1
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2019-05-07 18:10:57 -0700

    v.0.2.3 working crop module

--- deepclust_predict.py
+++ deepclust_predict.py
@@ -1,4 +1,6 @@
-import pickle
+
+import time
+
 import os
 import sys
 import numpy as np
@@ -8,17 +10,26 @@ from glob import glob
 import cv2
 import pandas as pd
 import matplotlib.pyplot as plt
+import torch
+import torch.backends.cudnn as cudnn
+from torch import nn
+from torch import optim
 import torch.utils.data
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
 
 
-# pip
-from stardist import StarDist
+import matplotlib.pyplot as plt
+
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+from torch.autograd import Variable
+
 
 
 # custom
-from starDist_predict import stardist_predict, PolyArea
-from clust_centroid import dbscan_alg, find_medoid
-from singleCell_export import slice_export
+from util import load_model
+from util import AverageMeter, Logger, UnifLabelSampler
 
 np.random.seed(6)
 lbl_cmap = random_label_cmap()
@@ -40,16 +51,30 @@ if len(sys.argv)==1:
     sys.exit(1)
 argsP = parser.parse_args()
 
+def main():
+    # load model
+    model = load_model(argsP.model)
+    model.cuda()
+    model.eval()
+
+
+
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+    test_transforms = transforms.Compose([transforms.Resize(224),
+                                          transforms.ToTensor(),
+                                          ])
+
+
+
+
+
+    #### Random chunks #####
 
 
 
 if __name__ == "__main__":
-    with open(os.path.join(argsP.model, 'cluster'), "rb") as f:
-        b = pickle.load(f, encoding='latin1')
-
-    # get image names in the directory
-    im_names = [os.path.basename(x) for x in sorted(glob(os.path.join(argsP.wd, '*.tif*')))]
-
 
 
 
