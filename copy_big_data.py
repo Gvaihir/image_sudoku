facb05f301efa9e055bb17d1da3714faa93b030e
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2019-05-19 22:43:00 -0700

    v0.0.3 copy module with random selection

--- copy_big_data.py
+++ copy_big_data.py
@@ -2,9 +2,10 @@ import shutil
 import os
 import sys
 import argparse
+import re
+import numpy as np
 from argparse import RawTextHelpFormatter
 from glob import glob
-import pandas as pd
 
 
 # arguments
@@ -14,7 +15,7 @@ parser = argparse.ArgumentParser(
     epilog='''Copy wisely''')
 parser.add_argument('-i', '--input', default=os.getcwd(), help='directory to copy from. Default - WD')
 parser.add_argument('--file', default=None, help='File name to copy. Default - NONE')
-parser.add_argument('--selection', default=None, type=str, help='STR Select specific portions of the screen. Accepts tab delimited list of plates/wells. See samplesheet. Default = NONE')
+parser.add_argument('--rnd', default=None, type=float, help='Select random cells? Accepts float[0.0:1.0] Default = NONE')
 requiredNamed = parser.add_argument_group('required named arguments')
 requiredNamed.add_argument('-o', '--output', help='Input file name', required=True)
 
@@ -29,15 +30,14 @@ if __name__ == "__main__":
 
     # use selection ?
     if argsP.selection != None:
-        selection_df = pd.read_csv(argsP.selection, sep='\t')
-        selection_df['well_row'] = 'r' + selection_df['well'].replace(r'\d', '', regex=True).apply(
-            lambda x: ord(x) - 64).apply(lambda x: format(x, '02d'))
-        selection_df['well_col'] = 'c' + selection_df['well'].replace(r'\D', '', regex=True).astype(int).apply(
-            lambda x: format(x, '02d'))
-        selection_df['sample'] = selection_df['well_row'].str.cat(selection_df['well_col'])
+        uniq_wells = np.unique([re.search(r'(Pt\d+_r\d+c\d+)', x)[0] for x in glob(os.path.join(argsP.input, '*tif'))])
 
-        # subset dfDir using only sample names from selection
-        dfDir = dfDir[dfDir["Sample_selection"].isin(selection_df['sample'])]
+        for i in uniq_wells:
+            all_files = glob(os.path.join(argsP.input, "".join([i, '*tif'])))
+            rand_select = np.random.choice(all_files, round(len(all_files) * argsP.rnd), replace=False)
+
+            for f in rand_select:
+                shutil.copy(f, argsP.output)
 
     for f in glob(os.path.join(argsP.input, "".join([argsP.file, '*tif']))):
         shutil.copy(f, argsP.output)
