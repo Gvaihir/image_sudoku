9b8dce7e2a05ae800794663c6428912d36e32ff3
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2019-08-02 19:20:27 -0700

    v0.4.0 added channel selection

--- image_merge.py
+++ image_merge.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
     inDir = listdir_nohidden(inPath)
     inDir.sort()
     dfDir = pd.DataFrame(inDir, columns=['Name'])
-    outPath = '/'.join([os.path.dirname(inPath), "converted_rgb"])
+    outPath = os.path.join(inPath, "converted_rgb")
     if not os.path.exists(outPath):
         os.makedirs(outPath)
 
