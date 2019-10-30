463547e04664b3369da3c112f75f15be712782ad
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2019-10-30 09:14:09 -0700

    v0.8.1 deployment of aae predictions

---
+++ aae_filter.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Copyright (c) 2018-present, Anton (Gvaihir) Ogorodnikov, Ye lab UCSF.
+conda activate imgSudoku
+
+IMG_WD=/home/aogorodnikov/classes_rgb_corrected
+MODELS=/home/aogorodnikov/aae
+BATCH=16
+OUT=/home/aogorodnikov/aae_filter_classes
+
+mkdir -p ${OUT}
+
+PYTHON="/home/aogorodnikov/anaconda3/envs/imgSudoku/bin/python"
+
+cwd="/sudoku/train"
+${PYTHON} aae_filter.py -i ${IMG_WD} -m ${MODELS} -b ${BATCH} \
+  -o ${OUT} -v
