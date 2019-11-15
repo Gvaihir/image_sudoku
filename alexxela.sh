f64fe3999ec1330fdf0cd12dfb0b4ab23c8d30e6
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2019-11-14 18:11:58 -0800

    v0.9.0 alex net in conv

---
+++ alexxela.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Copyright (c) 2018-present, Anton (Gvaihir) Ogorodnikov, Ye lab UCSF.
+source activate imgSudoku
+
+IMG_WD=/home/aogorodnikov/data_aae
+EPOCH=1500
+BATCH=128
+OUT=/home/aogorodnikov/aae_2Dlatent/
+latent_dim=2
+
+mkdir -p ${OUT}
+
+PYTHON="/home/aogorodnikov/anaconda3/envs/imgSudoku/bin/python"
+
+cwd="/sudoku/train"
+${PYTHON} aae.py -i ${IMG_WD} -e ${EPOCH} -b ${BATCH} \
+  -o ${OUT} -v --input_dim 104 104 3 --latent_dim $latent_dim \
+  --train --adversarial --conv
+
