c39141f7b02d322473a9b32041aeec447c8b7fdd
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2019-11-14 18:20:57 -0800

    v0.9.0 alex net in conv

--- alexxela.sh
+++ alexxela.sh
@@ -7,7 +7,7 @@ IMG_WD=/home/aogorodnikov/data_aae
 EPOCH=1500
 BATCH=128
 OUT=/home/aogorodnikov/alexxela/
-latent_dim=128
+latent_dim=64
 
 mkdir -p ${OUT}
 
@@ -16,5 +16,5 @@ PYTHON="/home/aogorodnikov/anaconda3/envs/imgSudoku/bin/python"
 cwd="/sudoku/train"
 ${PYTHON} aae_alexxela.py -i ${IMG_WD} -e ${EPOCH} -b ${BATCH} \
   -o ${OUT} -v --latent_dim $latent_dim \
-  --train --adversarial --conv --sobel
+  --train --adversarial --sobel
 
