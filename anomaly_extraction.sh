437fe682c37f6b8951d6559cc774d36073da9b11
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2019-11-21 11:13:39 -0800

    v0.10.3 anomaly extraction code

--- anomaly_extraction.sh
+++ anomaly_extraction.sh
@@ -10,5 +10,5 @@ mkdir -p ${OUT}
 
 PYTHON="/home/aogorodnikov/anaconda3/envs/imgSudoku/bin/python"
 
-${PYTHON} anomaly_exctraction.py -j ${JSON} -o ${OUT} -a 0.0062016 -d 0.75594133 \
+${PYTHON} anomaly_exctraction.py -j ${JSON} -o ${OUT} -a 0.007576 -d 0.7769362 \
   --example -v
