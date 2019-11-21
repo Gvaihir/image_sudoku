03438326b0fc046431d40ba589059ad151cc500f
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2019-11-20 19:19:53 -0800

    v0.10.3 anomaly extraction code

--- anomaly_extraction.sh
+++ anomaly_extraction.sh
@@ -10,15 +10,5 @@ mkdir -p ${OUT}
 
 PYTHON="/home/aogorodnikov/anaconda3/envs/imgSudoku/bin/python"
 
-${PYTHON} anomaly_extraction.py -j ${JSON} -o ${OUT} -a
-
-
-# Required parameters
-
-# Other parameters
-parser.add_argument('-a', '--ae_loss', default=0.00, type=float, help='Reconstruction error threshold. Default - 0.00')
-parser.add_argument('-d', '--adv_loss', default=0.00, type=float, help='Adversarial error threshold. Default - 0.00')
-parser.add_argument('--example', action='store_true', help='Prepare a grid of images that fall to Normal and Anomaly,'
-                                                           'according to thresholds')
-parser.add_argument('--and_logic', action='store_true', help='Thresholds are combined in AND logic. Default = OR logic')
-parser.add_argument('-v', '--verbose', action='store_true', help='Image generation mode from latent space')
+${PYTHON} anomaly_extraction.py -j ${JSON} -o ${OUT} -a 0.0054373 -d 0.7413648 \
+  --example -v
